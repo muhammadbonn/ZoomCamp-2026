52c0223ca94b12aa38ef4d181f55c76eff02d171
Muhammad Bonn <118687900+muhammadbonn@users.noreply.github.com>
2026-02-17 01:08:31 +0200

    Delete homework04/models directory

--- homework04/models/fct_monthly_zone_revenue.sql
+++
@@ -1,8 +0,0 @@
-select
-    year,
-    month,
-    pickup_location_id as zone_id,
-    sum(total_amount) as revenue,
-    count(*) as total_trips
-from {{ ref('fct_trips') }}
-group by year, month, pickup_location_id
--- homework04/models/fct_trips.sql
+++
@@ -1,5 +0,0 @@
-select
-    *,
-    extract(year from pickup_datetime) as year,
-    extract(month from pickup_datetime) as month
-from {{ ref('int_trips_unioned') }}
